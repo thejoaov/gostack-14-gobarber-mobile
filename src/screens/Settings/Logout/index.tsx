020cd29c9ecd3dec028ea4a56019b4ee157db23b
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2020-12-27 22:49:44 -0300

    feat(Settings): base screens

---
+++ src/screens/Settings/Logout/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'components'
+
+import { Text } from 'react-native'
+
+const Logout: React.FC = () => (
+  <Container>
+    <Text>Logout</Text>
+  </Container>
+)
+
+export default Logout
---
+++ src/screens/Settings/Profile/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'components'
+
+import { Text } from 'react-native'
+
+const Profile: React.FC = () => (
+  <Container>
+    <Text>Profile</Text>
+  </Container>
+)
+
+export default Profile
