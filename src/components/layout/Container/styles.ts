6e1d1712cc0afdf66dcee55463127d9e01953389
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2020-12-28 01:21:09 -0300

    refactor(components): add background color to Container

--- src/components/layout/Container/styles.ts
+++ src/components/layout/Container/styles.ts
@@ -1,9 +1,10 @@
 import styled from 'styled-components/native'
 import { space, layout, flexbox, position } from 'styled-system'
 
-export const View = styled.View`
+export const View = styled.SafeAreaView`
   ${space}
   ${layout}
   ${flexbox}
   ${position}
+  background-color: ${({ theme }) => theme.colors.background}
 `
