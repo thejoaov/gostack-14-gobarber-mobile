113877b1cd5a55f93111439faae4f8d1a23c5c34
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2020-12-27 22:45:43 -0300

    chore: update eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,80 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native-community',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      'babel-module': {},
+    },
+  },
+  env: { jest: true },
+
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  overrides: [
+    {
+      files: ['**/*.tsx'],
+      rules: {
+        'react/prop-types': 'off',
+      },
+    },
+  ],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx'] }],
+    'no-unused-expressions': 'off',
+    'no-use-before-define': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'no-console': ['error'],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'no-underscore-dangle': 'off',
+    'no-extra-boolean-cast': 'off',
+    'import/prefer-default-export': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    camelcase: [2, { properties: 'always' }],
+    complexity: ['error', { max: 5 }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: true, optionalDependencies: false, peerDependencies: false },
+    ],
+    'import/no-unresolved': [2, { ignore: ['StoreTypes$'] }],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+    ],
+    'react/boolean-prop-naming': [
+      'error',
+      {
+        propTypeNames: ['bool', 'mutuallyExclusiveTrueProps'],
+        rule: '^(is|has)[A-Z]([A-Za-z0-9]?)+',
+        message: 'It is better if your prop ({{ propName }}) matches this pattern: ({{ pattern }})',
+      },
+    ],
+  },
+}
