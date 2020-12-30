c2fb65a725e6adb9c6468d1aa8dc3a2b427c7d62
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2020-12-30 08:19:19 -0300

    refactor(SignIn): apply last components

--- src/screens/Auth/SignIn/index.tsx
+++ src/screens/Auth/SignIn/index.tsx
@@ -1,16 +1,28 @@
 import React from 'react'
-import { Container, Image, Text } from 'components'
+import { Button, Container, FooterButton, Image, LinkButton, Text, TextInput } from 'components'
 
 import logoImg from 'assets/images/logo.png'
 
 const SignIn: React.FC = () => (
-  <Container flex={1} center>
-    <Image source={logoImg} />
+  <>
+    <Container flex={1} paddingX={40} center>
+      <Image source={logoImg} />
 
-    <Text mt={64} mh={24} fontSize={24} variant="medium">
-      Faça seu login
-    </Text>
-  </Container>
+      <Text mt={64} mx={24} fontSize={24} variant="medium">
+        Faça seu login
+      </Text>
+
+      <TextInput mt={24} />
+
+      <TextInput mt={10} />
+
+      <Button title="Entrar" mt={12} />
+
+      <LinkButton title="Esqueci minha senha" mt={24} />
+    </Container>
+
+    <FooterButton title="Criar uma conta" />
+  </>
 )
 
 export default SignIn
