92fa801682e8c3814a5e30580a7fd236afca4b9a
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2020-12-30 05:20:49 -0300

    feat(nivel-03/iniciando-o-aplicativo-mobile): importando fontes externas - SignIn

--- src/screens/Auth/SignIn/index.tsx
+++ src/screens/Auth/SignIn/index.tsx
@@ -2,10 +2,15 @@ import React from 'react'
 import { Container, Image, Text } from 'components'
 
 import logoImg from 'assets/images/logo.png'
+import theme from 'core/styles/theme'
 
 const SignIn: React.FC = () => (
-  <Container>
+  <Container flex={1} alignItems="center" justifyContent="center">
     <Image source={logoImg} />
+
+    <Text fontSize={24} mt={64} color={theme.colors.white} variant="medium">
+      Faça seu login
+    </Text>
   </Container>
 )
 
