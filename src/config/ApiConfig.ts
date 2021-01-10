0bfa0923f1678e3d32cdc5f4c13e4555fbf4fa9f
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2021-01-10 12:29:40 -0300

    fix: api config

--- src/config/ApiConfig.ts
+++ src/config/ApiConfig.ts
@@ -3,5 +3,5 @@ import axios from 'axios'
 export const ApiConfig = axios.create({
   baseURL: __DEV__
     ? 'http://localhost:3333'
-    : 'https://gostack-11-server.herokuapp.com',
+    : 'https://gobarber-11-server.herokuapp.com',
 })
