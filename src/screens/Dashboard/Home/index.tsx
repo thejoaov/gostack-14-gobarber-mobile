62a4c7c6299115d0a537fd4059cae5b868b8bc9f
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2021-02-03 23:12:33 -0300

    feat: add margin to container in dashboard

--- src/screens/Dashboard/Home/index.tsx
+++ src/screens/Dashboard/Home/index.tsx
@@ -4,16 +4,16 @@ import { useAuth } from 'core/hooks/AuthContext'
 import { useTranslation } from 'react-i18next'
 import { Props } from './types'
 
-const Home: React.FC<Props> = () => {
+const Home: React.FC<Props> = ({ navigation }) => {
   const { signOut, user } = useAuth()
   const { t } = useTranslation('home')
 
   return (
     <>
       <Header greeting={t('greeting')} name={user?.name} />
-      <Container center>
+      <Container center marginX={30}>
         <Button
-          padding={20}
+          margin={20}
           onPress={() => signOut()}
           title={t('provisory_logout_button')}
         />
