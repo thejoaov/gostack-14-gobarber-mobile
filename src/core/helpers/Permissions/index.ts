386a7b91d10f30f66cbdabf4a9e9a23476ac8e93
João Victor dos Santos Mendes <jvictorsantos852@gmail.com>
2021-02-21 21:26:41 -0300

    feat: permission test

--- src/core/helpers/Permissions/index.ts
+++ src/core/helpers/Permissions/index.ts
@@ -1,15 +1,21 @@
 import { PermissionsAndroid, PermissionStatus } from 'react-native'
 import i18n from 'i18next'
+import Device from '../Device'
 
 const Permissions = {
-  requestCamera: (): Promise<PermissionStatus> =>
-    PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA, {
-      title: i18n.t('permissions.android.camera.title'),
-      message: i18n.t('permissions.android.camera.message'),
-      buttonNeutral: i18n.t('permissions.android.camera.buttonNeutral'),
-      buttonNegative: i18n.t('permissions.android.camera.buttonNegative'),
-      buttonPositive: i18n.t('permissions.android.camera.buttonPositive'),
-    }),
+  requestCamera: (): Promise<PermissionStatus> | null => {
+    if (Device.isAndroid()) {
+      return PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA, {
+        title: i18n.t('permissions.android.camera.title'),
+        message: i18n.t('permissions.android.camera.message'),
+        buttonNeutral: i18n.t('permissions.android.camera.buttonNeutral'),
+        buttonNegative: i18n.t('permissions.android.camera.buttonNegative'),
+        buttonPositive: i18n.t('permissions.android.camera.buttonPositive'),
+      })
+    }
+
+    return null
+  },
 }
 
 export default Permissions
